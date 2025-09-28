a17fef68654475670a448e7a8eb4993faa861a4a
santanaigot <santanaigoor01@gmail.com>
2025-09-28 18:15:24 -0300

    [feat] - styled component summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,31 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { SummaryContainer } from "./styles";
+import { SummaryCard, SummaryContainer } from "./styles";
+
 
 export function Summary() {
     return(
         <SummaryContainer>
-            <div>
+            <SummaryCard>
                 <header>
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
                 <strong>R$ 17.400,00</strong>
-            </div>
-             <div>
+            </SummaryCard>
+             <SummaryCard>
                 <header>
                     <span>Saidas</span>
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
                 <strong>R$ 17.400,00</strong>
-            </div>
-             <div>
+            </SummaryCard>
+             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
                 <strong>R$ 17.400,00</strong>
-            </div>
+            </SummaryCard>
         </SummaryContainer>
     )
 }
