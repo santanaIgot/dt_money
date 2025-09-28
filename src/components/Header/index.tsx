0e8e0742d1d834587c6ec4e909e7d637227d1478
santanaigot <santanaigoor01@gmail.com>
2025-09-28 17:52:06 -0300

    [feat] - styled component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,1 +1,14 @@
-    
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import LogoImg from '../../assets/Logo.svg'
+
+export function Header() {
+    return(
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={LogoImg} alt="" />
+                <NewTransactionButton>nova trasação</NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
---
+++ src/pages/Trasactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+
+export function Trasactions() {
+    return(
+        <div>
+            <Header/>
+            <Summary/>
+        </div>
+    )
+}
