662de8d179afc1a75b7b198ff3e5734d577cb7f9
santanaigot <santanaigoor01@gmail.com>
2025-10-15 17:32:38 -0300

    [improvment] - create transaction

--- src/contexts/TrasactionsContext.tsx
+++ src/contexts/TrasactionsContext.tsx
@@ -4,64 +4,86 @@
 import { createContext, useEffect, useState, type ReactNode } from "react";
 import { api } from "../lib/axios";
 
-interface TransactionProps{
-  id:number,
-  description: string,
-  type: 'income' | 'outcome',
-  price: number,
-  category: string,
-  createdAt: string,
+
+
+interface CreateTransactionInput {
+  description: string;
+  category: string;
+  price: number;
+  type: 'income' | 'outcome';
+}
+interface TransactionProps {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
 }
 
-
 interface TrasactionContextType {
-    transactions: TransactionProps[],
-    fetchTransactions: (query?: string) => Promise<void>;
+  transactions: TransactionProps[];
+  fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data:CreateTransactionInput) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 export const TransactionContext = createContext({} as TrasactionContextType);
 
-export function TrasactionsProvider({children}: TransactionsProviderProps) {
+export function TrasactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<TransactionProps[]>([]);
 
-    const [transactions, setTransactions] = useState<TransactionProps[]>([]);
-     
-      async function fetchTransactions(query?: string) {
-        //  const url = new URL('/trasactions')
+  async function fetchTransactions(query?: string) {
+    console.log("asdadsdsa", query);
+    //  const url = new URL('/trasactions')
 
-        //  //seta valor na url    
-        //  if(query){
-        //     url.searchParams.append('q', query)
-        //  }
+    //  //seta valor na url
+    //  if(query){
+    //     url.searchParams.append('q', query)
+    //  }
 
-        //  const response = await fetch(url);
-        //  const data = await response.json();
-    
-        //  console.log(data);
+    //  const response = await fetch(url);
+    //  const data = await response.json();
 
-        const response = await api.get('/transactions', {
-            params:{
-                _sort:'createdAt',
-                _order:'desc',
-                q:query
-            }
-        })
-         setTransactions( response.data)
-      }
-      useEffect(()=>{
-       fetchTransactions()
-      },[])
+    //  console.log(data);
 
+    const response = await api.get("/transactions", {
+      params: {
+        _sort: "createdAt",
+        _order: "desc",
+        q: query,
+      },
+    });
+    console.log("aa", response);
+    setTransactions(response.data);
+  }
 
+  async function createTransaction(data: CreateTransactionInput) {
+    const { category, description, price, type } = data;
+    const response = await api.post("/transactions", {
+      description,
+      category,
+      price,
+      type,
+      createdAt: new Date(),
+    });
+    setTransactions(state => [response.data, ...state ])
+  }
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
 
-    return(
-        <TransactionContext.Provider value={{
-            transactions, fetchTransactions
-        }}>
-            {children}
-        </TransactionContext.Provider>
-    )
+  return (
+    <TransactionContext.Provider
+      value={{
+        transactions,
+        fetchTransactions,
+        createTransaction
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  );
 }
